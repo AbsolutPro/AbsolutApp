093c459cedd38a247f7e35d708e70a2df166fea3
Joao Victor Cardoso dos Santos <jvcs.mecatronica@gmail.com>
2022-02-03 19:57:26 -0300

    teste de unidade para chat

--- unit.py
+++ unit.py
@@ -24,7 +24,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
+import re
 
 st.set_page_config(
 	 page_title='MKT',
@@ -38,9 +38,16 @@ st.set_page_config(
 	}
 )
 
+def listar_nomes(texto):
+	#print(f'O TEXTO \n\n\n {texto}') '' "" `` 
+	title_name = re.findall(r'"_3q9s6"><span dir="auto" title="(.*?)" class="g', texto)
+	if title_name:
+		return title_name
+	else:
+		return re.findall(r'<span dir="auto" title="(.*?)" class="gg', texto)
 
 
-def ui_automation():
+def ui_caminhar_chat():
 	#send_to_clipboard(f'imagem-0.png')
 	opts = Options()
 	opts.add_argument("--user-data-dir=C:\\Users\\Victor\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 4")
@@ -50,10 +57,42 @@ def ui_automation():
 	wait = WebDriverWait(driver, 60)
 
 	btn_search = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="side"]/div[1]/div/label/div/div[2]')))
-	#SEARCH_INPUT
+	btn_search.send_keys("")
+	chat_bloco = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="pane-side"]/div[2]')))
+	time.sleep(1)
+	#barra de busca
+	btn_search.send_keys(Keys.ARROW_DOWN)
+	#PRIMEIRA CONVERSA SELECIONADA
+
+	is_chat_not_end = True
+	while is_chat_not_end:
+		time.sleep(1)
+		ctt_selected = wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_2_TVt')))
+		selected_name = listar_nomes(str(ctt_selected.get_attribute('innerHTML')))
+
+		chat_bloco.send_keys(Keys.ARROW_DOWN)
+		print(f' \nSELECIONADO > {selected_name[0]}\n\n\n ')
+
+
+	time.sleep(3)
+
+
+def ui_buscar():
+	#send_to_clipboard(f'imagem-0.png')
+	opts = Options()
+	opts.add_argument("--user-data-dir=C:\\Users\\Victor\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 4")
+	driver = webdriver.Chrome(options=opts)#options=opts ---headless
+	driver.get('https://web.whatsapp.com/')
+	driver.maximize_window()
+	wait = WebDriverWait(driver, 60)
+
+	btn_search = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="side"]/div[1]/div/label/div/div[2]')))
 	btn_search.click()
 	btn_search.send_keys("Salvando")
 	time.sleep(1)
+	
+
+
 	btn_search.send_keys(Keys.ARROW_DOWN)
 	time.sleep(3)
 	ctt_selected = wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_2_TVt')))
@@ -70,6 +109,7 @@ def ui_automation():
 
 	actions = ActionChains(driver)
 	actions.key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+	time.sleep(10)
 
 
 st.markdown(literais.css_botao_boneca_russa, unsafe_allow_html=True)
@@ -80,8 +120,9 @@ with st.container():
 		
 		st.info('imagem anexada')
 
-	if st.button('confirma', on_click=ui_automation()):
+	if st.button('confirma',):
 		st.info('Executando Chrome')
+		ui_caminhar_chat()
 		
 
 
