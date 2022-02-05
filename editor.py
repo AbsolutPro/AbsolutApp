50c4dd064a8d0a0a70be1133f6069be1cf975d79
Joao Victor Cardoso dos Santos <jvcs.mecatronica@gmail.com>
2022-02-05 04:16:55 -0300

    editor online para AWS

--- editor.py
+++ editor.py
@@ -24,30 +24,24 @@ def retornar_lista_arquivo_python():
     cwd = os.getcwd()
     onlyfiles = [os.path.join(cwd, f) for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
 
-    print(f'DIRETORIO {cwd}\n {onlyfiles}')
+    #print(f'DIRETORIO {cwd}\n {onlyfiles}')
     return onlyfiles
 def file_leitor(arquivo):
-    with open(arquivo, 'r') as arqv:
+    with open(arquivo, 'r', encoding='utf-8') as arqv:
         tudo = arqv.read()
     return tudo
 
 with coluna_direita:
-    st.header("A dog")
+    st.header("Aws dog editor")
     arquivo_selecionado = st.selectbox('scripts', retornar_lista_arquivo_python())
     content = st_ace(value=file_leitor(arquivo_selecionado),height=400)
 
 with coluna_esquerda:
     st.header("Observador")
-    mostrar = st.button('mostar')
-    if mostrar:
-        st.code(content)
-        #chama essa funcao só de passagem, então ao ler o botao enviar e depois ler o botao confirmar em dois rounds de script
-        #quando o botao confirmar chamar sua rotina padrão on_click 
-        # o loop anterior onde o botao enviar estava ativado (porque foi ele[enviar] quem ativou o botao confirmar)
-        #vai continuar existindo. e por isso essa linha aqui funciona com  on_click
-        #mas não funciona dentro do contexto do botao confirmar.
-        if st.button('confirmar', on_click=print('retorna antes de ser acionado e se acionado e anterior nao, não executa')):
-            print('estanho?')
-            st.write(content)
-            #print(f'salvar no arquivo {content}') #aqui não vai ser executado quando você clicar esse botao
-            #
+    
+    salvar = st.button('salvar')
+    if salvar:
+        with open(arquivo_selecionado, 'w', encoding='utf-8') as arquivo:
+            arquivo.write(content)
+        
+    st.code(content)
